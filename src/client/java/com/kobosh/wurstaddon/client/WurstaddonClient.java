f243ac8b4bfa1e5f1d8b16261f7c812eae0ba57f
koboshchan <lucaszhang1001@gmail.com>
2026-04-19 17:34:46 -0700

    init

---
+++ src/client/java/com/kobosh/wurstaddon/client/WurstaddonClient.java
@@ -0,0 +1,10 @@
+package com.kobosh.wurstaddon.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WurstaddonClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/kobosh/wurstaddon/Wurstaddon.java
@@ -0,0 +1,10 @@
+package com.kobosh.wurstaddon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Wurstaddon implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
